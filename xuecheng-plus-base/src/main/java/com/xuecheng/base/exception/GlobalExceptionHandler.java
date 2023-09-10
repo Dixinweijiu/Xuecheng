64f2db38645c0087fae830a62e1786ce48609885
jxchen <956896784@qq.com>
2023-09-10 18:58:48 +0800

    上一个提交的新增课程与异常处理是9.9号的
    2023.9.10 异常过滤

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.xuecheng.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author jxchen
@@ -24,7 +30,7 @@ public class GlobalExceptionHandler {
     public RestErrorResponse customException(XueChengPlusException e){
 
         //记录异常
-        log.error("系统异常{}", e.getErrMessage(), e);
+        log.error("[系统异常]{}", e.getErrMessage(), e);
 
         //解析出异常信息
         String errMessage = e.getErrMessage();
@@ -39,10 +45,26 @@ public class GlobalExceptionHandler {
     public RestErrorResponse exception(Exception e){
 
         //记录异常
-        log.error("系统异常{}", e.getMessage(), e);
+        log.error("[系统异常]{}", e.getMessage(), e);
 
         //解析出异常信息
         RestErrorResponse restErrorResponse = new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrMessage());
         return restErrorResponse;
     }
+
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<String> msgList = new ArrayList<>();
+        //将错误信息放在msgList
+        bindingResult.getFieldErrors().stream().forEach(item->msgList.add(item.getDefaultMessage()));
+        //拼接错误信息
+        String msg = StringUtils.join(msgList, ",");
+        log.error("[系统异常]{}",msg);
+        return new RestErrorResponse(msg);
+    }
+
 }
---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,13 @@
+package com.xuecheng.base.exception;
+
+/**
+ * @author jxchen
+ * @version 1.0
+ * @description TODO
+ * @date 2023/9/10 14:26
+ */
+public class ValidationGroups {
+    public interface Insert{};
+    public interface Update{};
+    public interface Delete{};
+}
