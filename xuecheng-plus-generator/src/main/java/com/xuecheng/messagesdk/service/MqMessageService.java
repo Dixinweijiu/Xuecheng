7b0eadbbc745e90ad65fcda1e6dc52e01d469f13
jxchen <956896784@qq.com>
2023-10-10 15:02:04 +0800

    2023.10.10 消息SDK

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/messagesdk/service/MqMessageService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.messagesdk.service;
+
+import com.xuecheng.messagesdk.model.po.MqMessage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-09-26
+ */
+public interface MqMessageService extends IService<MqMessage> {
+
+}
